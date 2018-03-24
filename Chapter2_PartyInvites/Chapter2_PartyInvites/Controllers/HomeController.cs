bd290ad414b3dc254c06695e964c0ceb15d6909c
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-24 22:47:39 +0000

    Chapter 2: Your First MVC Application

---
+++ Chapter2_PartyInvites/Chapter2_PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Chapter2_PartyInvites.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter2_PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: store response from guest
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+
+            return View(guestResponse);
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(s => s.WillAttend == true));
+        }
+    }
+}
---
+++ Chapter2_PartyInvites/Chapter2_PartyInvites/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chapter2_PartyInvites.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
