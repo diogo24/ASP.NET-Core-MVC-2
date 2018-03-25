f3132f4122e11abd3caf91ec15c6a041e727f89d
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-25 15:07:30 +0100

    Chapter 5: Working with Razor

---
+++ Chapter5_Razor/Chapter5_Razor/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Chapter5_Razor.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter5_Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID   = 1,
+                Name        = "Kayak",
+                Description = "A boat for one person",
+                Category    = "Watersports",
+                Price       = 275M
+            };
+
+            ViewBag.StockLevel = 2;
+
+            return View(myProduct);
+        }
+
+        public IActionResult IndexLists()
+        {
+            Product[] array = {
+             new Product {Name = "Kayak", Price = 275M},
+             new Product {Name = "Lifejacket", Price = 48.95M},
+             new Product {Name = "Soccer ball", Price = 19.50M},
+             new Product {Name = "Corner flag", Price = 34.95M}
+             };
+            return View(array);
+        }
+    }
+}
