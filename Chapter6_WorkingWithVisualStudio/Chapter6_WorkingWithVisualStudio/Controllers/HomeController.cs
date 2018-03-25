a923b19e30ae9c97b9cea5ed79f91fe549709858
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-03-25 20:10:02 +0100

    Chapter 6: Working with Visual Studio

---
+++ Chapter6_WorkingWithVisualStudio/Chapter6_WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Chapter6_WorkingWithVisualStudio.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Chapter6_WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where(p => p?.Price < 50));
+    }
+}
