1bea1896ff935f07ee8a4a6a888e03354bbc3075
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-04-07 17:27:45 +0100

    Chapter 20: API Controllers

---
+++ Chapter20_APIControllers/Chapter20_APIControllers/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Chapter20_APIControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter20_APIControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository { get; set; }
+
+        public HomeController(IRepository repo) => repository = repo;
+
+        public ViewResult Index() => View(repository.Reservations);
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Chapter20_APIControllers/Chapter20_APIControllers/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Chapter20_APIControllers.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace Chapter20_APIControllers
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IRepository, MemoryRepository>();
+            services.AddMvc().AddXmlDataContractSerializerFormatters()                .AddMvcOptions(opts => {
+                    opts.FormatterMappings.SetMediaTypeMappingForFormat(format: "xml", contentType: new MediaTypeHeaderValue("application/xml"));
+                    opts.RespectBrowserAcceptHeader = true;
+                    opts.ReturnHttpNotAcceptable = true;
+                });        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
