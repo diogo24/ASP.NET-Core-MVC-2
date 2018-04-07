69df9402c1a7cec4694549673f2053b4d9f86440
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-04-07 19:51:11 +0100

    Chapter 21: Views

---
+++ Chapter21_Views/Chapter21_Views/Infrastructure/DebugDataViewEngine.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter21_Views.Infrastructure
+{
+    public class DebugDataViewEngine : IViewEngine
+    {
+        public ViewEngineResult GetView(string executingFilePath, string viewPath, bool isMainPage)
+        {
+            return ViewEngineResult.NotFound(viewPath,
+                new string[] { "(Debug View Engine - GetView)" });
+        }
+
+        public ViewEngineResult FindView(ActionContext context, string viewName, bool isMainPage)
+        {
+            if (viewName == "DebugData")
+            {
+                return ViewEngineResult.Found(viewName, new DebugDataView());
+            }
+            else
+            {
+                return ViewEngineResult.NotFound(viewName,
+                    new string[] { "(Debug View Engine - FindView)" });
+            }
+        }
+    }
+}
