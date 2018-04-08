61c062e871b10c90a9149c8fc1d785e3c1781e10
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-04-08 14:34:33 +0100

    Chapter 27: Model Validation

---
+++ Chapter27_ModelValidation/Chapter27_ModelValidation/Models/Appointment.cs
@@ -0,0 +1,26 @@
+﻿using Chapter27_ModelValidation.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter27_ModelValidation.Models
+{
+    public class Appointment
+    {
+        [Required]
+        [Display(Name = "name")]
+        public string ClientName { get; set; }
+
+        [UIHint("Date")]
+        [Required(ErrorMessage = "Please enter a date")]
+        [Remote("ValidateDate", "Home")]
+        public DateTime Date { get; set; }
+
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
+        public bool TermsAccepted { get; set; }
+    }
+}
