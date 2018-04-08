436e437c19c349f9e795cccf308eb120c28bec4f
Diogo Carlos Saramago Marques <diogo.marques@dcslsoftware.com>
2018-04-08 23:09:44 +0100

    Chapter 31: Model Conventions and Action Constraints

---
+++ Chapter31_ConventionsAndConstraints/Chapter31_ConventionsAndConstraints/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Chapter31_ConventionsAndConstraints.Infrastructure;
+using Chapter31_ConventionsAndConstraints.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chapter31_ConventionsAndConstraints.Controllers
+{
+    //[AdditionalActions]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+
+
+        [ActionName("Index")]
+        [UserAgent("Edge")]
+        public IActionResult Other() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Other)
+        });
+
+        //[ActionName("Details")]
+        //[ActionNamePrefix("Do")]
+        //[AddAction("Details")]
+        [UserAgent("Edge")]
+        public IActionResult List() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(List)
+        });
+    }
+}
